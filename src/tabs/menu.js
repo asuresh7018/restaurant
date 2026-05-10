c134350857331deed0cc8d46bc5f1340c33cd370
Arjun Suresh <asuresh7018@gmail.com>
2026-05-10 10:31:58 +0000

    Layout framework for setting up a menu

--- src/tabs/menu.js
+++ src/tabs/menu.js
@@ -1,6 +1,33 @@
+// Define menu here as a json for easier editing and make immutable
+const Menu = (function() {
+    let menuList = [
+    {"Title": "Burger1",
+    "Description": "Description1",
+    "Price": "Price1"}
+    ]
+
+    const GetMenu = () => {
+        return menuList;
+    }
+
+    return { GetMenu };
+})();
+
 function serveMenu() {
     const contentDiv = document.querySelector("#content");
-    contentDiv.innerHTML = "Menu Page";
+    contentDiv.innerHTML = `
+    <div class="header">Menu</div>
+    `;
+    createMenuItems(contentDiv);
+}
+
+function createMenuItems(contentDiv) {
+    for (const menuItem of Menu.GetMenu()) {
+        contentDiv.innerHTML += `
+        <div class="itemTitle">${menuItem.Title}</div>
+        <div class="itemDescription">${menuItem.Description}</div>
+        <div class="itemPrice">${menuItem.Price}</div>`
+    }
 }
 
 export default serveMenu;
